ae1323f8ac7a64a87d4daa8a79c0e1ae1903f4fd
Eman <emanuelray23@gmail.com>
2025-10-26 02:00:46 +0800

    updated some inccorect codes

--- Bit_Vector_Set/BitMask.c
+++ Bit_Vector_Set/BitMask.c
@@ -1,101 +1,93 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void display(unsigned char set);
 void initialize(unsigned char *set);
 void insert(unsigned char *set, int element);
-void deleteElem(unsigned char *set, int element);
+void delete(unsigned char *set, int element);
 bool find(unsigned char set, int element);
 unsigned char Union(unsigned char A, unsigned char B);
 unsigned char intersection(unsigned char A, unsigned char B);
 unsigned char difference(unsigned char A, unsigned char B);
+void display(unsigned char set);
+
+
+
 int main() {
-	
-	unsigned char A;
-	unsigned char B;
-	unsigned char C;
-	
-	initialize(&A);
-	insert(&A,1);
+    
+    unsigned char A;
+    unsigned char B;
+    unsigned char C;
+    initialize(&A);
+    
+    insert(&A,1);
     insert(&A,6);
+  
     
-    initialize(&B);
-    insert(&B,3);
-    insert(&B,6);
-    insert(&B,7);
+    display(A);
     
-    initialize(&C);
-    
-    C = Union(A,B);
-    
-	display(C);
-	
-	return 0;
+    return 0;
 }
 void initialize(unsigned char *set) {
-	
-	*set = 0;
+    
+     *set = 0;
+    
 }
-
 void insert(unsigned char *set, int element) {
-	
-	if(element <=8) {
-
-	unsigned int mask = 1 << (element-1);
-	  *set = *set | mask;
-	  
-	}
+    
+    if(element > 0 && element < 8) {
+        
+    unsigned int mask = 1 << (element-1);
+   
+   *set |= mask;
+    }
+   
 }
 
-void deleteElem(unsigned char *set, int element) {
-	
-		if(element>0 && element <=7) {
-		unsigned int mask = 1 << (element-1);
-	    
-		*set = *set& ~mask; //~mask is flipping the mask so if mask 101 then ~mask is 010
-			
-		}
+void delete(unsigned char *set, int element) {
+    
+    if(element > 0 && element < 8) {
+        
+      unsigned int mask = 1 << (element-1);
+   *set &= ~mask;  
+   
+    }
+    
 }
-
-unsigned char Union(unsigned char A, unsigned char B) {
-	
-	return A | B;
-}
-
-unsigned char intersection(unsigned char A, unsigned char B) {
-	
-	return A & B;
-}
-
 bool find(unsigned char set, int element) {
-	
-	
-	unsigned int mask = 1 << (element-1);
-	
-	set & mask;
+    
+    if(element > 0 && element < 8) {
+         unsigned int mask = 1 << (element-1);
+    set & mask;
+    }
+   
+   
 }
-
+unsigned char Union(unsigned char A, unsigned char B) {
+    
+    return A | B;
+    
+}
+unsigned char intersection(unsigned char A, unsigned char B) {
+    
+    return A & B;
+      
+}
 unsigned char difference(unsigned char A, unsigned char B) {
-	
-	return A & ~B;
+    
+    return A&~B;
+    
 }
-
 void display(unsigned char set) {
-	
-	int bits = sizeof(char)*8;
-    int i=1;   
-       unsigned int mask = 1;
-       
-       printf("{");
-       while(mask!=0) {
-       	if(set & mask) {
-       		printf("%d, ",i);
-		   }
-		   i++;
-       	mask = mask <<1;
-	   }
-	   printf("}");
-	   
-	   
-	   
+ 
+int i = 1;
+unsigned int mask = 1;
+printf("{");
+while(mask!=0) {
+    if(set & mask){
+        printf("%d,",i);
+    }
+    i++;
+    mask = mask << 1;
+    }
+    printf("}");
 }
